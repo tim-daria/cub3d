de97e5ea7aeb47c737fa60c421980116e57726a2
tjllis <floydjellis@gmail.com>
2025-10-06 22:22:38 +0200

    chore: merged cub3d.h files; deleted duplicate libft folder chore: headers cleanup

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -15,5 +15,41 @@
 
 # define WIDTH 1024
 # define HEIGHT 768
+# define BLOCK_SIZE 64
+
+typedef struct s_map {
+	char	**map;
+	int		block_height;
+	int		block_width;
+}	t_map;
+
+// typedef struct s_textures {
+// 	t_position	player;
+// 	t_position	exit;
+// 	t_position	collectable;
+// }	t_textures;
+
+typedef struct s_mlx_data {
+	//t_textures	textures;
+	t_map		map;
+	void		*mlx_connection;
+	void		*mlx_win;
+	void		*img;
+	int			img_width;
+	int			img_height;
+	//int			count_movements;
+}	t_mlx_data;
+
+//void	draw_map(t_mlx_data *data);
+//int		handle_movements(int keysym, t_mlx_data *data);
+int		count_player(t_map map);
+int		map_height(int fd);
+int		end_program(t_mlx_data *data);
+void	init_data(t_mlx_data *data);
+void	free_map(char **map, int row);
+int		check_characters(t_map *map);
+int		check_mapfilename(char *filename);
+int		check_walls(t_map *map);
+
 
 #endif
